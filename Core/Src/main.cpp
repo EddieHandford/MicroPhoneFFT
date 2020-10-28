0b96da5fe6ec525d14ff52b916f4b4a1d5645945
c.goodman <charlesgoodman.95@gmail.com>
2020-10-28 22:39:09 +0000

    no message

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -29,11 +29,7 @@
 #define SCL_TIME 0x01
 #define SCL_FREQUENCY 0x02
 #define SCL_PLOT 0x03
-<<<<<<< HEAD
-#define silly comment 1994
-=======
-#define HELLO EDDIE
->>>>>>> master
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
